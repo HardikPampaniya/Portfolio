68e8bfc332aa42d2abce9773e65e7cc97e329d53
N Master <nmaster@netclues.lan>
2024-05-17 19:28:03 +0530

    portfolio

---
+++ app/(sections)/projects/constant.ts
@@ -0,0 +1,34 @@
+export const ProjectData = [
+  {
+    title: "Dynamic Form Builder",
+    description:
+      "A MERN stack application where users can perform CRUD operation on form",
+    techstack: ["MERN Stack", "React", "Node", "Express", "MySQL"],
+    live: "https://findyourjob.vercel.app",
+    github: "https://github.com/HardikPampaniya/Dynamic-Form-Builder",
+  },
+  {
+    title: "Hotstar Clone",
+    description:
+      "I built a Hotstar clone, focusing solely on the front-end.",
+    techstack: ["HTML", "CSS", "Javascript"],
+    live: "https://hotstar-duplicate.vercel.app/",
+    github: "https://github.com/HardikPampaniya/Hotstar-Clone-Using-HTML-CSS-JS",
+  },
+  {
+    title: "Notes",
+    description:
+      "A React-based note-taking app for creating, editing, and deleting notes.",
+    techstack: ["React", "Node", "Express", "MongoDB", "Tailwind CSS"],
+    live: "https://notes-hardik.vercel.app/notes",
+    github: "https://github.com/HardikPampaniya/Notes",
+  },
+  {
+    title: "Temprature Converter",
+    description:
+      "Converts Celsius, Fahrenheit, Kelvin. Intuitive, reliable.",
+    techstack: ["HTML", "CSS", "Javascript"],
+    live: "https://notes-hardik.vercel.app/notes",
+    github: "https://github.com/HardikPampaniya/Temperature-Converter/tree/main",
+  },
+];
