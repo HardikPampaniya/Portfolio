f7b2582c499ee06330847ee457e5ae5b0a596c96
Hardik Pampaniya <98203155+HardikPampaniya@users.noreply.github.com>
2024-05-21 11:56:14 +0530

    insert new section of project chat-app

--- app/(sections)/projects/constant.ts
+++ app/(sections)/projects/constant.ts
@@ -1,8 +1,16 @@
 export const ProjectData = [
   {
+    title: "Chat-App",
+    description:
+      "This web-app provides a seamless and interactive chat experience",
+    techstack: ["MERN Stack", "React", "Node", "Express", "MongoDB"],
+    live: "https://chat-realtime-app.vercel.app/login",
+    github: "https://github.com/HardikPampaniya/Chat-App",
+  }
+  {
     title: "Dynamic Form Builder",
     description:
-      "A MERN stack application where users can perform CRUD operation on form",
+      "A MERN stack web-application where users can perform CRUD operation on form",
     techstack: ["MERN Stack", "React", "Node", "Express", "MySQL"],
     live: "https://dynamic-form-builder-webapp.vercel.app/",
     github: "https://github.com/HardikPampaniya/Dynamic-Form-Builder",
@@ -26,7 +34,7 @@ export const ProjectData = [
   {
     title: "Temperature Converter",
     description:
-      "A basic temperature converter website that convert the temprature into celsius, fahrenheit and kelvin ",
+      "A basic temperature converter website that convert the temprature into celsius, fahrenheit and kelvin",
     techstack: ["HTML", "CSS", "Javascript"],
     live: "https://temp-converter-webapp.vercel.app/",
     github: "https://github.com/HardikPampaniya/Temperature-Converter",
